7d9b8741a194084f0678c6dd9238fd2b01ca6a98
Jay-Jay23 <skosanajojo@gmail.com>
2023-07-25 12:40:09 +0200

    created a random bill amount function and function to sum up total bill

---
+++ scripts/dinnerTotal.py
@@ -0,0 +1,14 @@
+import random
+
+def total_bill(amount, tip_percentage):
+    tip_payable = amount * tip_percentage
+    sum_owed = amount + tip_payable
+    return sum_owed
+
+def random_bill_amount():
+    return random.uniform(10, 200)
+
+print("Here is your Bill")
+dinner_bill = random_bill_amount()
+
+print(f"dinner amount is: {dinner_bill}" )
