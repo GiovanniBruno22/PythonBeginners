800fbd46d954b4ec7fbcf2a8c34469ecb102f6ba
Jay-Jay23 <skosanajojo@gmail.com>
2023-07-25 13:01:09 +0200

    added user input for amount payable and if statement for when user enters an amount lower than total due amount

--- scripts/dinnerTotal.py
+++ scripts/dinnerTotal.py
@@ -1,7 +1,7 @@
 import random
 
 def total_bill(amount, tip_percentage):
-    tip_payable = amount * tip_percentage
+    tip_payable = amount * tip_percentage/100
     sum_owed = amount + tip_payable
     return sum_owed
 
@@ -14,4 +14,18 @@ dinner_bill = random_bill_amount()
 print(f"dinner amount is: {dinner_bill}" )
 
 user_tip = float(input("How much would you like to tip? "))
+total_due =total_bill(dinner_bill,user_tip)
+
+print(f"Total due (including tip): {total_due}")
+
+amount_payable =float(input("Enter amount you are paying with:"))
+
+if amount_payable >= total_due:
+    change = amount_payable - total_due
+    print(f"Thank you for your payment. Your change is: {change}")
+else:
+    print("The amount paid is insufficient. Please pay the full amount.")
+
+
+
 
