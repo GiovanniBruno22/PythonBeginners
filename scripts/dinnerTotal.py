f1560f2d343de785ade4c550c224f8764920c4a3
Jay-Jay23 <skosanajojo@gmail.com>
2023-07-26 11:14:28 +0200

    adding docstring for each function

--- scripts/dinnerTotal.py
+++ scripts/dinnerTotal.py
@@ -1,15 +1,31 @@
 import random
 
 def total_bill(amount, tip_percentage):
+    '''
+    Calculate the total amount due for dinner, including the tip.
+
+    Args: amount (float): The total amount for dinner before adding the tip.
+        tip_percentage (float): The tip percentage to be applied.
+
+    Returns float: The total amount due, including the tip.
+    '''
     tip_payable = amount * tip_percentage/100
     sum_owed = amount + tip_payable
     return sum_owed
 
 def random_bill_amount():
+    '''
+    Generate a randomized total amount for dinner. 
+    returns float: A randomized dinner amount between $10 and $200
+    '''
     return round(random.uniform(10, 200),2)
 
 
 def main():
+    '''
+    This function generates a randomized dinner amount, takes user input for the tip percentage,
+    calculates the total amount due, and shows the change to be given back to the user.
+    '''
     print("Here is your Bill")
     dinner_bill = random_bill_amount()
 
