855eeed7b4171ede2203646ef6f68dfc6a84d669
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-08-04 12:16:47 +0300

    Caesar Cipher Added

---
+++ scripts/Caesar Cipher.py
@@ -0,0 +1,45 @@
+Cipher = {
+  "A": "X",
+  "B": "Y",
+  "C": "Z",
+  "D": "A",
+  "E": "B",
+  "F": "C",
+  "G": "D",
+  "H": "E",
+  "I": "F",
+  "J": "G",
+  "K": "H",
+  "L": "I",
+  "M": "J",
+  "N": "K",
+  "O": "L",
+  "P": "M",
+  "Q": "N",
+  "R": "O",
+  "S": "P",
+  "T": "Q",
+  "U": "R",
+  "V": "S",
+  "W": "T",
+  "X": "U",
+  "Y": "V",
+  "Z": "W"
+}
+
+if __name__ == "__main__":
+
+  print("Please enter a text to cipher: ")
+  text = input()
+  output = ""
+
+  for a in text:
+    if a.isalpha():
+      if a.islower():
+        output += Cipher[a.upper()].lower()
+      else:
+        output += Cipher[a.upper()]
+    else:
+      output += a
+
+  print("Your ciphered message is: ", output)
