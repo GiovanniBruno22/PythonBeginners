12735e701bc3f27ed9d66263c410f86895bcfc69
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-11-18 18:40:54 -0500

    Update randPassGenerate.py

--- scripts/randPassGenerate.py
+++ scripts/randPassGenerate.py
@@ -1,14 +1,21 @@
+"""Password generator"""
+
 import secrets
 import string
+import sys
     
 
 def main():
+    """Main generator for the program"""
+    try:
+        length: int = int(input("Enter the length of the password: ")) # Taking user input about the length of password
+    except ValueError:
+        print("Not a valid number.")
+        sys.exit()
 
-    passCodeLength = int(input("Enter the length of the password: "))   # Taking user input about the length of password
+    alphabet: string = string.ascii_letters + string.digits + string.punctuation # character to be used in password
 
-    alphabet = string.ascii_letters + string.digits + string.punctuation    # Defining alphanumeric and special characters to be used in password
-
-    password = ''.join(secrets.choice(alphabet) for i in range(passCodeLength))     # Creating the password
+    password: str = "".join(secrets.choice(alphabet) for i in range(length)) # generating the password
 
     print("Password is: " + password)
 
